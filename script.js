f3b06a23a059a215661b86a354d602749c8bc18a
HelderQRZ <helderqrz.dev@gmail.com>
2022-08-31 23:01:41 -0300

    Organização de Conteúdo

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ function aleatorio() {
     document.body.style.background = `rgb(${r}, ${g}, ${b})`
 
 }
+
 function vermelho() {
     document.body.style.background = 'rgb(228, 57, 57)'
 }
-function azul() {
-    document.body.style.background = 'rgb(83, 84, 173)'
-}
+
 function verde() {
     document.body.style.background = 'rgb(90, 231, 24)'
 }
+
+function azul() {
+    document.body.style.background = 'rgb(83, 84, 173)'
+}
+
 function branco() {
     document.body.style.background = 'rgb(255,255,255)'
 }
